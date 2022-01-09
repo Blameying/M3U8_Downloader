e0f2b1ef701cc46c387e0be6791c9f01737a5823
Blameying <xblame@qq.com>
2022-01-09 11:31:56 +0800

    M3U8 simple multi-thread downloader initial commit

---
+++ src/main.rs
@@ -0,0 +1,146 @@
+use clap::{arg, App};
+use std::{fs, process, path::Path, io::Read, thread, sync::{mpsc::{self, Receiver, Sender}, Arc}};
+use regex::{Regex};
+use reqwest::{self, header::{HeaderName, HeaderValue}};
+use url::{Url};
+use pbr::ProgressBar;
+use serde_json;
+
+#[derive(Debug)]
+struct M3U8 {
+    path: String,
+    base_url: String,
+    output: String,
+    header: String
+}
+
+impl M3U8 {
+    fn new(path: &str, base_url: &str, output: &str, header: &str) -> M3U8 {
+        M3U8 {
+            path: String::from(path),
+            base_url: String::from(base_url),
+            output: String::from(output),
+            header: String::from(header)
+        }
+    }
+
+    fn parse_header(&self) -> Vec<(String, String)> {
+        let mut headers: Vec<(String, String)> = vec![];
+        if self.header.is_empty() {
+            headers
+        } else {
+            let f = fs::read_to_string(&self.header).unwrap();
+            let v: serde_json::Value = serde_json::from_str(&f).unwrap();
+            match v {
+                serde_json::Value::Object(ref map) => {
+                    for (k, v) in map {
+                        if let serde_json::Value::String(ref s) = v {
+                            headers.push((String::from(k), String::from(s)));
+                        }
+                    }
+                },
+                _ => ()
+            }
+            println!("header: {:?}", headers);
+            headers
+        }
+    }
+
+    fn load_m3u8(path: &str) -> Vec<String> {
+        let content = fs::read_to_string(path).unwrap_or_else(|op| {
+            println!("File {:?} read failed, error: {}", path, op);
+            process::exit(0);
+        });
+
+        let mut list = vec![];
+        let ts_match = Regex::new(r"[a-zA-Z0-9]+\.ts").unwrap();
+        for line in content.lines() {
+            // Get encrypt key file.
+            if let Some(pos) = ts_match.find(line) {
+                list.push(String::from(pos.as_str()));
+            }
+        }
+        list
+    }
+
+    fn download_ts<'a>(base_url: &str, list: &[String], tx: &Sender<(String, Vec<u8>)>, header: &[(String, String)])
+    {
+        let client = reqwest::blocking::Client::new();
+
+        for ts in list {
+            let url = Url::parse(base_url).unwrap().join(&ts).unwrap();
+            let mut body = client.get(url.as_str());
+            for h in header {
+                body = body.header(HeaderName::from_bytes(&h.0.as_bytes()).unwrap(), 
+                             HeaderValue::from_bytes(&h.1.as_bytes()).unwrap());
+            }
+            let body = body.send().unwrap().bytes().unwrap();
+            let content: Result<Vec<_>, _> = body.bytes().collect();
+            if let Ok(data) = content {
+                tx.send((String::from(ts), data)).unwrap();
+            }
+        }
+    }
+
+    pub fn download(&self, thread_num: i32) {
+        if !Path::new(&self.output).exists() {
+            fs::create_dir_all(&self.output).unwrap();
+        }
+        let list = M3U8::load_m3u8(&self.path);
+        let mut thread_pool: Vec<thread::JoinHandle<_>> = vec![];
+        let iter = list.chunks(list.len() / (thread_num as usize));
+        let (tx, rx): (Sender<(String, Vec<u8>)>, Receiver<(String, Vec<u8>)>) = mpsc::channel();
+        let output_ref = self.output.clone();
+        let total = list.len();
+        thread_pool.push(thread::spawn( move || {
+            let mut pb = ProgressBar::new(total as u64);
+            for data in rx {
+                let path = Path::new(&output_ref).join(&data.0);
+                fs::write(path, data.1).unwrap();
+                pb.inc();
+            }
+            pb.finish_print("Done!");
+        }));
+        
+        let base_url = Arc::new(self.base_url.clone());
+        let header = Arc::new(self.parse_header());
+        for i in iter {
+            let data = i.to_vec();
+            let tx = tx.clone();
+            let base_url = Arc::clone(&base_url);
+            let header = Arc::clone(&header);
+            thread_pool.push(thread::spawn( move || {
+                M3U8::download_ts(&base_url, &data, &tx, &header);
+                drop(tx);
+            }));
+        }
+
+        drop(tx);
+
+        for t in thread_pool {
+            t.join().unwrap();
+        }
+    }
+}
+
+fn main() {
+    let matches = App::new("Soo!")
+        .version("1.0")
+        .author("XBlame <xblame@qq.com>")
+        .about("Multi-thread m3u8 downloader")
+        .arg(arg!(-f --file <FILE> "the local path of the m3u8 file").required(true))
+        .arg(arg!(-u --url  <URL> "the url of the m3u8 file").required(true))
+        .arg(arg!(-d --dest <DIR> "the path of output dir").required(false))
+        .arg(arg!(-j --j <N> "multi-thread number, default: 8").required(false))
+        .arg(arg!(--header <JSON_FILE> "http request header, you can input a json file to declare it.").required(false))
+        .get_matches();
+
+    let file_path = matches.value_of("file").unwrap();
+    let url = matches.value_of("url").unwrap();
+    let dest = matches.value_of("dest").unwrap_or("./");
+    let thread_num: i32 = matches.value_of_t("j").unwrap_or(8);
+    let header = matches.value_of("header").unwrap_or("");
+
+    let config: M3U8 = M3U8::new(file_path,url, dest, header);
+    config.download(thread_num);
+}
